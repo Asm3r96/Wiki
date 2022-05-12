3004bb70b5f94a7002200854575609e28d94ff7b
Asm3r96 <104586162+Asm3r96@users.noreply.github.com>
2022-05-12 13:48:49 +0200

    functions are done
    still i have to edit the code to be like i want it to be

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('wiki/<str:title>', views.entry, name="entry"),
-    path("editPage",views.editPage, name='editPage'),
     path("search", views.search, name="search"),
+    path("newPage", views.newPage, name="newPage"),
+    path("save", views.save, name="save"),
+    path("randomPage",views.randomPage, name='randomPage'),
+    path("editPage",views.editPage, name='editPage'),
+    path("saveEdit",views.saveEdit, name='saveEdit')
 ]
